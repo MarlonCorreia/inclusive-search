68ce55257c8e756c9fc8119193ca078f016a7c62
MarlonCorreia <malilocsimas26@gmail.com>
2021-04-04 14:35:42 -0300

    start function implementation

--- src/reversedIndex.cpp
+++ src/reversedIndex.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <vector>
 #include "reversedIndex.hpp"
+#include "utils/utils.hpp"
+
+using namespace std;
+
+void ReversedIndex::addToindex(std::string phrase, int id)
+{
+    vector<string> words = split(phrase, " ");
+    
+    for(int i=0; i < words.size(); i++)
+    {
+        //implementation
+    }
+}   
