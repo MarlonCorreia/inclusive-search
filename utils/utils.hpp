67789d6b702ff46631750038c235ca89a47ad794
MarlonCorreia <malilocsimas26@gmail.com>
2021-04-04 14:32:44 -0300

    split utils

---
+++ utils/utils.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> split(string phrase, string delimiter)
+{
+    vector<string> words;
+
+    // implementation
+
+    return words;
+}
