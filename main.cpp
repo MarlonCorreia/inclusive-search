c1ae4e362b1e038a1b865078f748645de921536a
marloncorreia <malilocsimas26@gmail.com>
2021-04-04 23:59:39 -0300

    clean main

--- main.cpp
+++ main.cpp
@@ -1,39 +1,14 @@
 #include <iostream>
 #include "src/trie.hpp"
 #include <fstream>
+#include <vector>
+#include "src/reversedIndex.hpp"
 
 #define LOG(x) std::cout << x << std::endl
 
 int main()
 {
-    Trie* root = new Trie();
-
-    std::fstream MyFile("words/words.txt");
-    std::string word;
-    
-    while (std::getline(MyFile, word))
-    {   
-        root->addWord(word);
-        //LOG(word);
-    }
-    
-    while (true)
-    {   
-        std::string input;
-        std::cin >> input;
-        
-        std::vector<std::string> words = root->wordsByPrefix(input);
-
-        for (int i=0; i< words.size(); i++)
-        {
-        std::cout << i+1 << " -> " << words[i] << std::endl;
-        }
-        
-    }
-    
-    
-    delete root;
-
+    //socket listeners to be implemented here =]
     return 0;
 
 }
